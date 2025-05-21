3b70ca30a55160a5e18d8d8ca8ef1177b3f16429
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2025-05-21 21:18:44 +0000

    feat: refactor random wallet generation and remove unused function

--- src/RandomWallet.hs
+++ src/RandomWallet.hs
@@ -1,7 +1,5 @@
 module RandomWallet
-    ( randomWalletIO
-    -- , normalizeWallet
-    , randomWallet
+    ( randomWallet
     ) where
 
 import Wallet
@@ -11,22 +9,33 @@ import Wallet
     )
 
 import System.Random
-import Control.Monad 
+import System.Random.Shuffle
 import Numeric.LinearAlgebra
 
--- Create a random n-sized vector and turns into a Wallet
--- The function takes a random generator and a size n
--- and returns a random Wallet
-randomWalletIO :: StdGen -> Int -> IO Wallet
-randomWalletIO gen n = do
-    let randomShares = take n $ randomRs (0.0, 1.0) gen
+
+-- Built with the assistance of ChatGPT (GPT-4.1). Prompt:
+-- Generates an n-size list of floats where each value is randomly chosen between 0 and 0.2
+-- but does not exceed the remaining sum (starting from 1). The sum of the list is equal 1.
+-- At the end, the list is shuffled randomly to avoid any specific order.
+randomDpubleListN :: Int -> IO [Double]
+randomDpubleListN n = do
+    ll <- aux n 1 []
+    shuffleM ll
+    where
+        aux 0 _ xs = return xs
+        aux n rem xs = do
+            let maxVal = min 0.2 rem
+            x <- if n == 1
+                then return rem
+                else randomRIO (0, maxVal)
+            xs' <- aux (n - 1) (rem - x) (x : xs)
+            return xs'
+
+randomWallet :: Int -> IO Wallet
+randomWallet n = do
+    randomShares <- randomDpubleListN n
     let wallet = newWallet randomShares
     let walletNormalized = wallet / scalar (sumElements wallet)
     if validateWallet walletNormalized
         then return wallet
-        else randomWalletIO gen n
-
-randomWallet :: Int -> IO Wallet
-randomWallet n = do
-    gen <- newStdGen
-    randomWalletIO gen n
+        else randomWallet n
