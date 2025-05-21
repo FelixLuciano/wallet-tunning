e1d86cc743affb2c3da7976146da4a30e7132915
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2025-05-21 23:12:53 +0000

    refactor: clean up wallet-related imports and remove unused functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,23 @@
 module Main (main) where
 
 import Wallet
-    ( Wallet(..)
-    , newWallet
-    , newStockPricesHistory
+    ( newStockPricesHistory
     , validateWallet
-    , walletDalyReturns
-    , walletYearlyMeanReturn
-    , walletVolatility
+    , walletSharpeRatio
     )
 
 import RandomWallet
     ( randomWallet
     )
 
+import Numeric.LinearAlgebra
+
 main :: IO ()
 main = do
     myWallet <- RandomWallet.randomWallet 4
     putStrLn $ "My wallet: " ++ show myWallet
+
+    putStrLn $ "Walet assets Shares: " ++ show (sumElements myWallet)
 
     let isValid = validateWallet myWallet
     putStrLn $ "Is the wallet valid? " ++ show isValid
@@ -27,11 +27,5 @@ main = do
                                                      [110, 220, 320, 420]
                                                    ]
 
-    let returns = walletDalyReturns myWallet stockPrices
-    putStrLn $ "Wallet daily returns: " ++ show returns
-
-    let yearlyReturns = walletYearlyMeanReturn myWallet stockPrices
-    putStrLn $ "Wallet mean yearly return: " ++ show yearlyReturns
-
-    let volatility = walletVolatility myWallet stockPrices
-    putStrLn $ "Wallet volatility: " ++ show volatility
+    let volatility = walletSharpeRatio myWallet stockPrices
+    putStrLn $ "Sharpe Ratio: " ++ show volatility
