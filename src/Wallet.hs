14002c1fc77af871ec0f22595d111c41ba6f46b7
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2025-05-28 22:23:27 +0000

    refactor: improve code formatting and organization in Wallet module

--- src/Wallet.hs
+++ src/Wallet.hs
@@ -2,7 +2,10 @@ module Wallet
     ( AssetShare
     , Wallet
     , StockPrice
+    , StockPriceHistory
+    , StockPricesHistory
     , ReturnRate
+    , ReturnRateHistory
     , newWallet
     , newStockPricesHistory
     , validateWallet
@@ -16,26 +19,26 @@ module Wallet
     ) where
 
 import Numeric.LinearAlgebra
-  ( Matrix
-  , Vector
-  , fromColumns
-  , fromList
-  , fromLists
-  , scalar
-  , size
-  , subVector
-  , sumElements
-  , toColumns
-  , tr
-  , toList
-  , (<>)
-  , (#>)
-  )
+    ( Matrix
+    , Vector
+    , fromColumns
+    , fromList
+    , fromLists
+    , scalar
+    , size
+    , subVector
+    , sumElements
+    , toColumns
+    , tr
+    , toList
+    , (<>)
+    , (#>)
+    )
 
 import Control.Parallel.Strategies
-  ( parMap
-  , rpar
-  )
+    ( parMap
+    , rpar
+    )
 
 type AssetShare = Double
 type Wallet = Vector AssetShare
@@ -51,20 +54,20 @@ newWallet :: [Double] -> Wallet
 newWallet w = fromList w
 
 newStockPricesHistory :: [[StockPrice]] -> StockPricesHistory
-newStockPricesHistory p = fromLists p
+newStockPricesHistory = tr . fromLists
 
 validateWallet :: Wallet -> Bool
 validateWallet wallet = allSharesValid && totalSharesValid
-  where
-    allSharesValid = size wallet < 5 || all (<= 0.2) (toList wallet)
-    totalSharesValid = abs (sumElements wallet - 1.0) < 1e-9
+    where
+        allSharesValid = size wallet < 5 || all (<= 0.2) (toList wallet)
+        totalSharesValid = abs (sumElements wallet - 1.0) < 1e-9
 
 stockReturn :: StockPriceHistory -> ReturnRateHistory
 stockReturn prices = currentPrice / previousPrice - 1.0
-  where
-    n = size prices - 1
-    currentPrice = prefixVector 1 $ subVector 1 n prices
-    previousPrice = prefixVector 1 $ subVector 0 n prices
+    where
+        n = size prices - 1
+        currentPrice = prefixVector 1 $ subVector 1 n prices
+        previousPrice = prefixVector 1 $ subVector 0 n prices
 
 stockReturns :: StockPricesHistory -> Matrix ReturnRate
 stockReturns prices = fromColumns $ parMap rpar stockReturn $ toColumns prices
@@ -74,8 +77,8 @@ walletDalyReturns wallet prices = stockReturns prices #> wallet
 
 walletYearlyMeanReturn :: Wallet -> StockPricesHistory -> ReturnRate
 walletYearlyMeanReturn wallet prices = (sqrt 252) * mean returns
-  where
-    returns = walletDalyReturns wallet prices
+    where
+        returns = walletDalyReturns wallet prices
 
 stockPriceVolatility :: StockPriceHistory -> Vector Double
 stockPriceVolatility prices = prices - (scalar $ mean prices)
@@ -85,19 +88,19 @@ stockPricesVolatility prices = fromColumns $ parMap rpar stockPriceVolatility $ 
 
 walletVolatility :: Wallet -> StockPricesHistory -> Double
 walletVolatility wallet prices = sqrt . sumElements $ wallet * sigma #> wallet
-  where
-    v = stockPricesVolatility prices
-    n = fromIntegral (size wallet - 1)
-    sigma = tr v Numeric.LinearAlgebra.<> v / n
+    where
+        v = stockPricesVolatility prices
+        n = fromIntegral (size wallet - 1)
+        sigma = tr v Numeric.LinearAlgebra.<> v / n
 
 walletYearlyVolatility :: Wallet -> StockPricesHistory -> Double
 walletYearlyVolatility wallet prices = (sqrt 252) * walletVolatility wallet prices
 
 walletSharpeRatio :: Wallet -> StockPricesHistory -> Double
 walletSharpeRatio wallet prices = return / volatility
-  where
-    return = walletYearlyMeanReturn wallet prices
-    volatility = walletYearlyVolatility wallet prices
+    where
+        return = walletYearlyMeanReturn wallet prices
+        volatility = walletYearlyVolatility wallet prices
 
 -- UTILS
 
@@ -105,5 +108,5 @@ prefixVector x v = fromList (x : toList v)
 
 mean :: Vector Double -> Double
 mean x = sumElements x / n
-  where
-    n = fromIntegral (size x - 1)
+    where
+        n = fromIntegral (size x - 1)
