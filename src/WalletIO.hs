3131b6d903f52bbd8db3f488cf77c1ffd98b3d8c
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2025-05-28 22:23:38 +0000

    feat: implement WalletIO module for file reading and wallet generation

--- src/WalletIO.hs
+++ src/WalletIO.hs
@@ -1,11 +1,26 @@
-module RandomWallet
-    ( randomWallet
+module WalletIO
+    ( readFilePriceList
+    , readFilePricesHistory
+    , randomWallet
+    , randomWallets
     ) where
 
 import Wallet
-    ( Wallet(..)
+    ( Wallet
+    , StockPrice
+    , StockPricesHistory
     , newWallet
     , validateWallet
+    , newStockPricesHistory
+    )
+
+import Control.Parallel.Strategies
+    ( parMap
+    , rpar
+    )
+
+import Control.Monad
+    ( replicateM
     )
 
 import System.Random
@@ -21,6 +36,20 @@ import Numeric.LinearAlgebra
     , sumElements
     )
 
+
+readFilePriceList :: FilePath -> IO [StockPrice]
+readFilePriceList filePath = do
+    content <- readFile filePath
+    return $ parMap rpar read $ lines content
+
+readFilePricesHistory :: FilePath -> [String] -> IO StockPricesHistory
+readFilePricesHistory basePath tickers = do
+    histories <- mapM (\t -> readFilePriceList $ basePath ++ "/" ++ t ++ ".txt") tickers
+    let lens = map length histories
+    if all (== head lens) lens
+        then return $ newStockPricesHistory histories
+        else error $ "readFilePricesHistory: mismatched price history lengths: " ++ show (zip tickers lens)
+
 -- Built with the assistance of ChatGPT (GPT-4.1). Prompt:
 -- Generates an n-size list of floats where each value is randomly chosen between 0 and 0.2
 -- but does not exceed the remaining sum (starting from 1). The sum of the list is equal 1.
@@ -35,7 +64,7 @@ randomDoubleListN n = do
             let maxVal = min 0.2 rem
             x <- if n == 1
                 then return rem
-                else randomRIO (0, maxVal)
+                else randomRIO (0.001, maxVal)
             xs' <- aux (n - 1) (rem - x) (x : xs)
             return xs'
 
@@ -47,3 +76,7 @@ randomWallet n = do
     if validateWallet walletNormalized
         then return wallet
         else randomWallet n
+
+randomWallets :: Int -> Int -> IO [Wallet]
+randomWallets n l = do
+    replicateM l (randomWallet n)
