8899d0ac9228e5398eaadfab8ac709784094d6c5
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2025-06-05 05:54:34 +0000

    feat: update stock selection logic and integrate Tunning module for improved performance

--- app/Main.hs
+++ app/Main.hs
@@ -9,18 +9,52 @@ import TickersIO
     , readFilePricesHistory
     )
 
-import TunningIO
-    ( tuneTickersIO
+import Tunning
+    ( tuneTickers
     )
+
+import Data.Time.Clock.POSIX
+    ( getPOSIXTime
+    )
+
+
+import Control.Concurrent
+    ( getNumCapabilities
+    )
+
+import Control.Concurrent.QSem
+    ( newQSem
+    , waitQSem
+    , signalQSem
+    )
+
+import Control.Exception
+    ( bracket_
+    )
+
+mainSeed :: Int
+mainSeed = 24
 
 main :: IO ()
 main = do
     tickers <- TickersIO.readFileTickers "./data/index/US30-tickers.txt"
     prices <- TickersIO.readFilePricesHistory "./data/prices" tickers
 
-    let selections = getStocksSelection 2 tickers prices
-    putStrLn $ "Amount of possible selections: " ++ show (length selections)
+    let selections = getStocksSelection 3 tickers prices
+    let size = length selections
+    putStrLn $ "Amount of possible selections: " ++ show size
 
-    (myWallet, sharpeRatio) <- TunningIO.tuneTickersIO 100 selections
+    numCaps <- getNumCapabilities
+    sem <- newQSem numCaps
+    let withSemaphore = bracket_ (waitQSem sem) (signalQSem sem)
+
+    startTime <- getPOSIXTime
+    (sharpeRatio, myWallet, myTickers) <- Tunning.tuneTickers 1000 mainSeed selections withSemaphore
+    endTime <- getPOSIXTime
+
+    let elapsed = round (endTime - startTime) :: Integer
+
+    putStrLn $ "Elapsed time: " ++ show elapsed ++ " s"
     putStrLn $ "Sharpe Ratio: " ++ show sharpeRatio
+    putStrLn $ "Tickers: " ++ show myTickers
     putStrLn $ "Wallet: " ++ show myWallet
